cb36d7444bf26731bfa9f93bb48de594fc984aa1
Erik Altink <erik.altink@gmail.com>
2016-11-26 22:14:38 +0100

    Committed changes to plot1.R and added plot2.R to the repository

--- plot2.R
+++ plot2.R
@@ -10,9 +10,14 @@
 power_consumption <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
 power_feb <- power_consumption[(power_consumption$Date=="1/2/2007" | power_consumption$Date=="2/2/2007" ), ]
 
+# format date and time
+# The date and time are printed in the local format
+power_feb$Date <- as.Date(power_feb$Date, format="%d/%m/%Y")
+dateTime <- paste(power_feb$Date, power_feb$Time)
+power_feb$DateTime <- as.POSIXct(dateTime)
+
 # Plot the histogram to a PNG file
-png("plot1.png", width = 480, height = 480, units = "px")
-hist(power_feb$Global_active_power,xlab="Global Active Power (kilowatts)", main="Global Active Power",c="red")
-hist(power_feb$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", c="red")
+png("plot2.png", width = 480, height = 480, units = "px")
+plot(power_feb$Global_active_power~power_feb$DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
 dev.off()
 # Don't forget to close the device
